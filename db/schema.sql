b987ec801146d3ca52860f80821062d2da21d5d6
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-22 18:03:28 -0500

    added seeds.sql and manager_id

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,30 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-id: INT NOT NULL AUTO_INCREMENT,
-name: VARCHAR(30),
-PRIMARY KEY(id)
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE role (
-id: INT NOT NULL AUTO_INCREMENT,
-title: VARCHAR(30) NOT NULL,
-salary: DECIMAL,
-department_id: INT,
-PRIMARY KEY(id),
-FOREIGN KEY(department_id) REFERENCES department(id)
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
 
 CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
 
-id: INT NOT NULL AUTO_INCREMENT,
-first_name: VARCHAR(30) NOT NULL,
-last_name: VARCHAR(30) NOT NULL,
-role_id: INT,
+    manager_id INT default NULL,
 
-manager_id: INT default null,
---to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 
-PRIMARY KEY(id),
-FOREIGN KEY(role_id) REFERENCES role(id)
-)
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee(
+   first_name, last_name, manager_id
+) VALUES (
+    'Jim',
+    'Pickens',
+    null
+),(
+    'Kevin',
+    'Smith',
+    1
+)
