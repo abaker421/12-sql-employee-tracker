f8761dcbf560db3eaffde2aaffc44a22886c4a84
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-21 21:27:02 -0500

    added schema.sql syntax

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,33 @@
-department
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-id: INT PRIMARY KEY
+USE employees_db;
 
-name: VARCHAR(30) to hold department name
+CREATE TABLE department (
+id: INT NOT NULL AUTO_INCREMENT,
+name: VARCHAR(30),
+PRIMARY KEY(id)
+)
 
-role
+CREATE TABLE role (
+id: INT NOT NULL AUTO_INCREMENT,
+title: VARCHAR(30) NOT NULL,
+salary: DECIMAL,
+department_id: INT,
+PRIMARY KEY(id),
+FOREIGN KEY(department_id) REFERENCES department(id)
+)
 
-id: INT PRIMARY KEY
+CREATE TABLE employee (
 
-title: VARCHAR(30) to hold role title
+id: INT NOT NULL AUTO_INCREMENT,
+first_name: VARCHAR(30) NOT NULL,
+last_name: VARCHAR(30) NOT NULL,
+role_id: INT,
 
-salary: DECIMAL to hold role salary
+manager_id: INT default null,
+--to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 
-department_id: INT to hold reference to department role belongs to
-
-employee
-
-id: INT PRIMARY KEY
-
-first_name: VARCHAR(30) to hold employee first name
-
-last_name: VARCHAR(30) to hold employee last name
-
-role_id: INT to hold reference to employee role
-
-manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+PRIMARY KEY(id),
+FOREIGN KEY(role_id) REFERENCES role(id)
+)
