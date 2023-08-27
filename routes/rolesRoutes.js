5ff5ed0b5ebd03d7d00019f6eb8230dd7fd4f7aa
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-27 18:26:46 -0500

    changed file structure, removed routes, added userSelectionFunctions.js

--- routes/rolesRoutes.js
+++
@@ -1,4 +0,0 @@
-const router= require('express').Router()
-const connection = require('../db/conneciton.js')
-
-module.exports= router
---
+++ utils/userSelectionFunctions.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer')
+
+const viewAllDepartments= () => {
+
+}
+
+const viewAllRoles= () => {
+
+}
+
+const viewAllEmployees= () => {
+
+}
+const addDepartment= () => {
+
+}
+const addRole = () => {
+
+}
+const addEmployee= () => {
+
+}
+
+const updateEmployeeRole= () => {
+
+}
+
+module.exports = userSelectionFuctions
