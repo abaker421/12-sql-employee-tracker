ce3612a4b193a6a567426a4e6d7ad0ee2afb250f
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-27 18:19:09 -0500

    added switch functionality to inquirer.js

--- utils/inquirer.js
+++ utils/inquirer.js
@@ -7,7 +7,29 @@ inquirer.prompt({
     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
     default: 'View all departments'
 })
-.then((answers) => {
-console.log(answers)
+.then((userSelection) => {
+    switch(userSelection) {
+        case "View all departments":
+            console.log('Success')
+            break;
+        case "View all roles":
+            console.log('')
+            break;
+        case "View all employees":
+            console.log('')
+            break;
+        case "Add a department":
+            console.log('')
+            break;
+        case 'Add a role':
+            console.log('')
+            break;
+        case 'Add an employee':
+            console.log('')
+            break;
+        case 'Update an employee role':
+            console.log('')
+            break;}
+}) .catch( (err) => {
+    console.error(err)
 })
-
