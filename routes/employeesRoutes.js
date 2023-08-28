a1e14f9c102ed091061c752624acc933f2c2e260
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-27 19:23:48 -0500

    added viewAllEmployees

--- routes/employeesRoutes.js
+++ routes/employeesRoutes.js
@@ -1,4 +1,16 @@
 const router= require('express').Router()
-const connection = require('../db/conneciton.js')
+const connection = require('../db/connection.js')
+
+const viewAllEmployees = () => {
+    console.log('All Employees:')
+    const query= `SELECT * FROM employees`
+    connection.query(query, (err, rows) => {
+        if (err) {console.log(err)}
+        console.table(rows)
+    })
+    console.log()
+}
+
+viewAllEmployees()
 
 module.exports= router
