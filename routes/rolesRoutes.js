338407c24670e88cb2fa178486552d23777dbb77
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-27 19:56:31 -0500

    added viewAllRoles function

--- routes/rolesRoutes.js
+++ routes/rolesRoutes.js
@@ -1,4 +1,16 @@
 const router= require('express').Router()
 const connection = require('../db/conneciton.js')
+const inquirer= require('inquirer')
+
+const viewAllRoles = () => {
+    console.log('All Roles:')
+    let query= `SELECT * FROM roles`
+    connection.query(query, (err, rows) => {
+        if (err) {console.log(err)}
+        console.table(rows)
+    })
+    console.log()
+}
+
 
 module.exports= router
