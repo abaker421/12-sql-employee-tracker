3493a104923578d1b7c9e6bc7b67c972e1ced999
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-08-27 20:11:37 -0500

    updated file structure, added index.js, removed routes, added userSelectionFunctions

--- utils/userSelectionFunctions.js
+++ utils/userSelectionFunctions.js
@@ -1,6 +1,32 @@
 const router= require('express').Router()
 const connection = require('../db/connection.js')
-const inquirer= require('inquirer')
+const inquirer = require('inquirer')
+
+const viewAllDepartments = () => {
+    console.log('All Departments:')
+    let query= `SELECT * FROM departments`
+    connection.query(query, (err, rows) => {
+        if (err) {console.log(err)}
+        console.table(rows)
+    })
+    console.log()
+}
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {name: 'name',
+        type: 'input',
+        message: "Name the department to be added:"}
+    ])
+    .then(function(answer) {
+    let query= `INSERT INTO departments (name) VALUES (?)`
+    connection.query(query, [answer.name], 
+    function(err, res, fields){
+        if (err) throw err
+        console.log('Department Successfully Added!')
+    })
+})
+}
 
 const viewAllEmployees = () => {
     console.log('All Employees:')
@@ -78,6 +104,44 @@ const updateEmployee = () => {
             function (err, res, fields) {
                 if (err) throw err
                 console.log("Employee updated successfully!")
+            })
+    })
+}
+
+const viewAllRoles = () => {
+    console.log('All Roles:')
+    let query= `SELECT * FROM roles`
+    connection.query(query, (err, rows) => {
+        if (err) {console.log(err)}
+        console.table(rows)
+    })
+    console.log()
+}
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of the new role?'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the starting salary for the new role?'
+        },
+        {
+            name: 'department_id',
+            type: 'input',
+            message: 'What is the ID number of the deparment oif the role?'
+        }
+    ])
+    .then(function(answer){
+        let query = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`
+        connection.query(query, [answer.title, answer.salary, answer.department_id],
+            function (err, res, fields) {
+                if (err) throw err
+                console.log('Role Added Successfully!')
             })
     })
 }
